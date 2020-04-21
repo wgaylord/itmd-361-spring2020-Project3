dba1b3e4366136b2fa17e68e6e75409d69e6cda8
William Gaylord <chibill110@gmail.com>
2020-04-21 17:54:32 -0500

    Add google map api key (Restricted to my github page.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function initMap() {
+    // The location of IIT
+    var IIT = { lat: 41.831349, lng: -87.627269 };
+    // The map, centered at Bourg-Saint-Maurice
+    var map = new google.maps.Map(document.getElementById('map'),{'center':IIT});
+
+
+}
